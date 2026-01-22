cc36a4b58ad6e0f97b0c183c7c4816ca22e7b30b
JayCodes <www.amujonathan@gmail.com>
2026-01-22 03:55:51 +0100

    task: done & working perfectly.

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,27 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import ProfilePage from "./components/ProfilePage";
+import UserProfile from "./components/UserProfile";
+import WelcomeMessage from "./components/WelcomeMessage";
+import UserContext from "./Context/UserContext";
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  return (
+    <>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+
+      <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
+
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
+    </>
+  );
+}
+
+export default App;
---
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import UserContext from "../Context/UserContext";
+
+function UserDetails() {
+  const user = useContext(UserContext);
+  return (
+    <div>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;

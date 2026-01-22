6d99788b8b6c1f7451ecf302fa84c2dd8a94dcbd
JayCodes <www.amujonathan@gmail.com>
2026-01-22 02:59:06 +0100

    task: done with the tasks.

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#3b1219",
+        height: "70px",
+        color: "white",
+        display: "flex",
+        alignItems: "end",
+        paddingLeft: "10px",
+        paddingBottom: "10px",
+        borderBottom: "#b0354b solid 4px",
+      }}
+    >
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main >
+      <p style={{ borderBottom: "2px navy solid", marginLeft: '20px', fontSize: "18px" }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;

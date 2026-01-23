96bfd4f77e9e7bfe7f2ee122fe68adc272d7c2d2
JayCodes <www.amujonathan@gmail.com>
2026-01-23 05:09:56 +0100

    done & dusted.

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,30 +1,24 @@
 import { Link } from "react-router-dom";
 
 function Navbar() {
-    return ( 
-        <nav>
-            <li>
-                <Link to = "/">
-                Home
-                </Link>
-            </li>
-            <li>
-                <Link to = "/about">
-            About
-                </Link>
-            </li>
-            <li>
-                <Link to = "/services">
-                Our Services
-                </Link>
-            </li>
-            <li>
-                <Link to = "/contact">
-                Contact Us
-                </Link>
-            </li>
-        </nav>
-     );
+  return (
+    <nav style={{ backgroundColor: "gray", display: "flex", justifyContent: "center" }}>
+      <ul style={{ display: "flex", gap: "1rem", listStyle: "none", padding: 0, margin: 0 }}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/services">Our Services</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact Us</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;

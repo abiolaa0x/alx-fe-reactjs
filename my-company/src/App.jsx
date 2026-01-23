3f9abbc4996f47638a0af870c47b8d871a0e4743
JayCodes <www.amujonathan@gmail.com>
2026-01-23 04:47:21 +0100

    done & dusted.

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,10 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Services from "./components/pages/Services";
-import Contact from "./components/pages/Contact";
+import Home from "./components//Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components//Contact";
 
 function App() {
   return (

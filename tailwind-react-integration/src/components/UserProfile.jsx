686a7a90433c53a9d97be9c72e5a4c767e16a99b
JayCodes <www.amujonathan@gmail.com>
2026-02-19 10:50:27 +0100

    feature: add responsive style to user card component to be adaptable to different device sizes.

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,14 +1,18 @@
-function UserProfile () {
+function UserProfile() {
   return (
-    <div className="user-profile bg-gray-300 max-w-xs sm:max-w-sm mx-auto my-20 p-4 sm:p-8 rounded-lg shadow-lg">
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full mx-auto w-24 h-24 sm:w-36 sm:h-36" />
-      <h1  className="text-lg sm:text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-sm sm:text-base">
+    <div className="user-profile bg-gray-300 max-w-xs md:max-w-sm mx-auto my-20 p-4 md:p-8 rounded-lg shadow-lg">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="User"
+        className="rounded-full mx-auto w-24 h-24 md:w-36 md:h-36"
+      />
+      <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
     </div>
   );
-};
+}
 
 export default UserProfile;
